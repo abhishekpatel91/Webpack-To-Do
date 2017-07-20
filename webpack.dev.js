9393678b8de54a8a6336555d4d12f8121ea742ec
abhishekpatel91 <patelabhishek7892@gmail.com>
2017-07-20 17:40:59 +0530

    exclude node modules

--- webpack.dev.js
+++ webpack.dev.js
@@ -36,6 +36,7 @@ module.exports = {
             }
         }, {
             test: /\.less$/,
+            exclude: /(node_modules)/,
             use: [{
                 loader: "style-loader"
             }, {
